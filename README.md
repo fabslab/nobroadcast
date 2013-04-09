df82d4cae90fc48a4fa378b25534ec471a90cbd0
Fabien <fsbrookedev@gmail.com>
2013-04-09 00:29:36 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Source for http://teaser.nobroadcast.com
---
+++ scripts/main.js
@@ -0,0 +1,39 @@
+//This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// This function creates an <iframe> (and YouTube player)
+// after the API code downloads.
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('teaser', {
+    height: '620',
+    width: '880',
+    videoId: 'WWQOvs3qmkk',
+    playerVars: {
+      controls: 0,
+      showinfo: 0
+    },
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
+    }
+  });
+}
+
+// The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+  event.target.playVideo();
+}
+
+// The API calls this function when the player's state changes.
+function onPlayerStateChange(event) {
+
+}
+
+function stopVideo() {
+  player.stopVideo();
+}
