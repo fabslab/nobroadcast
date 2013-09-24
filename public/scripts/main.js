0608dd57590954a76fb89589332139687d87f26d
Fabien <fabienbrooke@gmail.com>
2013-09-24 19:30:35 -0400

    create album promo site

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,42 +1,118 @@
-$(function() {
-  $('.play').click(function() {
-    $(this).toggleClass('play').toggleClass('pause');
-  });
+(function() {
+  var imgFolder = 'images/waveforms/', audioFolder = 'audio/';
 
-  $(window).resize(function() {
-    var player = $('.player');
-    player.css({
-      left: ($(window).width() - player.outerWidth()) / 2,
-      top: ($(window).height() - player.outerHeight()) - 100
-    });
-  });
+  var track = new Audio();
 
-  $(window).resize();
+  var codecRegex = /^no|maybe$/;
+  var codecs = {
+    opus: !!track.canPlayType('audio/ogg; codecs="opus"').replace(codecRegex,''),
+    ogg: !!track.canPlayType('audio/ogg; codecs="vorbis"').replace(codecRegex,''),
+    mp3: !!track.canPlayType('audio/mpeg;').replace(codecRegex,'')
+  };
 
-});
+  var ext = codecs.opus ? '.opus' : codecs.ogg ? '.ogg' : codecs.mp3 ? '.mp3' : '';
+  track.src = 'audio/1' + ext;
 
-// setup the audio context
-window.playAlbum = (function playAlbum() {
-
-  // create audio element for first track
+  var playButton, waveBase, waveCover, prevButton, nextButton, songEls;
 
 
+  // Web audio here
 
   var AudioContext = window.AudioContext || window.webkitAudioContext;
 
-  if (!AudioContext) {
-    // Web Audio API not supported
-    // TODO: create audio element and play without visualization
-    return;
+  if (AudioContext) {
+    window.addEventListener('load', function() {
+      var context = new AudioContext();
+      var source = context.createMediaElementSource();
+      var analyzer = context.createAnalyzerNode();
+      source.connect(analyser);
+      analyzer.connect(context.destination);
+    });
   }
 
-  var context = new contextClass();
-
-  // TODO: receive binary stream of FLAC audio for each track
-  // decode it and run it through an analyser node to create visualization on canvas element
-  // before sending it to the destination to play it
-
-  // set up playback controls
 
 
+
+  document.addEventListener('DOMContentLoaded', function() {
+
+    playButton = document.getElementById('play');
+    prevButton = document.getElementById('prev');
+    nextButton = document.getElementById('next');
+    waveBase = document.getElementById('base');
+    waveCover = document.getElementById('cover');
+    songEls = document.getElementById('songs').children;
+
+    document.addEventListener('keydown', function(e) {
+      if (e.keyCode == 32) togglePlayback();
+      else if (e.keyCode == 37) prev();
+      else if (e.keyCode == 39) next();
+    });
+
+    playButton.addEventListener('click', togglePlayback);
+    prevButton.addEventListener('click', prev);
+    nextButton.addEventListener('click', next);
+
+    for (var i = 0; i < songEls.length; ++i) {
+      songEls[i].addEventListener('click', handleSongClick);
+    }
+
+    track.addEventListener('ended', next);
+
+    track.addEventListener('timeupdate', function() {
+      var percentComplete = ((this.currentTime / this.duration) * 100);
+      waveCover.style.width = percentComplete + '%';
+    });
+
+  });
+
+  function prev() {
+    playNextNumTrack(-1);
+  }
+
+  function next() {
+    playNextNumTrack(1);
+  }
+
+  function playNextNumTrack(num) {
+    var active = document.getElementsByClassName('active')[0];
+    var trackNum = parseInt(active.id) + num;
+    var songEl = document.getElementById(trackNum.toString());
+    if (songEl) {
+      active.className = '';
+      songEl.className = 'active playing';
+      playTrack(trackNum);
+    } else if (track.ended) {
+      active.className = '';
+    }
+  }
+
+  function togglePlayback() {
+    var active = document.getElementsByClassName('active')[0];
+    ~active.className.indexOf('playing') ? active.className = 'active' : active.className = 'active playing';
+    playButton.className ? playButton.className = '' : playButton.className = 'pause';
+    track.paused || track.ended ? track.play() : track.pause();
+  }
+
+  function handleSongClick() {
+    var playing = document.getElementsByClassName('playing')
+      , active = document.getElementsByClassName('active')[0];
+    if (active.id == this.id) {
+      togglePlayback();
+    } else {
+      active.className = '';
+      this.className = 'active playing';
+      playTrack(this.id);
+    }
+  }
+
+  function playTrack(num) {
+    var src = audioFolder + num + ext;
+    if (~track.src.indexOf(src)) return;
+    track.src = src;
+    playButton.className = 'pause';
+    waveBase.src = imgFolder + num + '-bg.png';
+    waveCover.style.width = 0;
+    waveCover.style.backgroundImage = 'url(' + imgFolder + num + '.png)';
+    track.play();
+  }
 })();
--- server.js
+++ server.js
@@ -1,13 +1,9 @@
-var express = require('express'),
-    http = require('http'),
-    path = require('path');
+var connect = require('connect')
+  , http = require('http')
+  ,  path = require('path');
 
-var app = express();
-
-app.set('port', process.env.PORT || 3000);
-  
-app.use(express.static(path.join(__dirname, 'public')));
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+var app = connect().use(connect.static(path.join(__dirname, 'public')));
+var port = process.env.PORT || 3000;
+http.createServer(app).listen(port, function() {
+  console.log("Server listening on port " + port);
 });
